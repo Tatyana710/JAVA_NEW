30e87195d093d67c70c2c7c2f9e7a2c6a8853fb3
Tatyana710 <64249431+Tatyana710@users.noreply.github.com>
2020-06-19 17:15:51 +0300

    Create task_2.2.java

---
+++ Myclass/task_2.2.java
@@ -0,0 +1,11 @@
+/*Задать пустой целочисленный массив резмером 8. С помощью цикла заполнить его
+значениями 0 3 6 9 12 15 18 21.*/
+
+public class Myclass {
+    public static void main(String[] args) {
+        int[] arr= new int[8];
+        for(int i=0;i<=21;i+=3){
+         System.out.print(i + " ");
+        }
+        }
+    }
