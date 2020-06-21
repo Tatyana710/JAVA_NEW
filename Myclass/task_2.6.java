1df3c4e053767f2d35e755b052b0398273867986
Tatyana710 <64249431+Tatyana710@users.noreply.github.com>
2020-06-21 17:11:38 +0300

    Create task_2.6.java

---
+++ Myclass/task_2.6.java
@@ -0,0 +1,28 @@
+/*Написать метод, в который передаётся не пустой одномерный целочисленный массив, метод должен
+вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны.
+Примеры: checkBalance ([1,1,1 || 2,1]) -> true, checkBalance([2,1,1,2,1])-> false,
+checkBalance([10||10]) -> true, граница показана символом ||, эти символы в массив не входят*/
+
+public class Myclass {
+    public static void main(String[] args){
+        boolean nums = checkBalance(new int[]{1,1,1});//не пойму как здесь поставить символ || чтобы сравнить левый и правый массив
+        System.out.println(nums);
+    }
+
+    private static boolean checkBalance(int[] array) {
+        int leftSum = 0;
+        for (int i = 0; i < array.length; i++) {
+            leftSum += array[i];
+            int rightSum = 0;
+            for (int j = 0; j < array.length; j++) {
+                rightSum += array[j];
+                if (array[i] > array[j]) array[j] = array[i];
+            }
+            if (leftSum == rightSum)
+                return true;
+        }
+        return false;
+    }
+}
+
+
